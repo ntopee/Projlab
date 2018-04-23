f62d44d1cccae69c06a5e9b5567af776eb1bf544
1nco <37378176+1nco@users.noreply.github.com>
2018-04-23 02:16:56 +0200

    Merge branch 'master' of https://github.com/ntopee/Projlab
    # Conflicts:
    #	src/Program/Main.java

--- src/Program/Beta_Test.java
+++ src/Program/Beta_Test.java
@@ -1,4 +1,41 @@
 package Program;
 
+import java.util.ArrayList;
+
 public class Beta_Test {
+
+    //Game.Init("Map.txt");
+    private Game game;
+    private ArrayList<String> levels;
+    //game.Init(levels.get());
+
+    public Beta_Test(){
+        levels = game.getLevels();
+
+    }
+
+
+    public void StartLevel(int i){
+        game.Init(levels.get(i));
+    }
+
+    public void Move(String player , Direction d){
+
+        game.GetCurrentLevel();
+    }
+
+    public void List(){
+
+    }
+
+    public void Put(){
+
+    }
+
+    public void EventHandler(){
+
+    }
+
+
+
 }
