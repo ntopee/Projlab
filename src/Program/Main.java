7ef35aa9589081c92a407810988edf878e436752
AcidicNut <37378049+AcidicNut@users.noreply.github.com>
2018-05-13 22:04:03 +0200

    8===D

--- src/Program/Main.java
+++ src/Program/Main.java
@@ -12,42 +12,6 @@ import java.io.InputStreamReader;
 
 public class Main extends  Application{
 
-    static public void Menu() throws IOException { // menu megvalositasa
-
-        while (true) {
-            System.out.println("Alpha teszt : A\n" +
-                    "Beta teszt: B\n" +
-                    "Exit: E\n" +
-                    "Please enter the right character, then hit Enter.");
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-            int i = 0;
-            try {
-                i = br.read();
-
-            } catch (NumberFormatException nfe) {
-                System.err.println("Invalid Format!");
-            }
-            char c = (char) i;
-            switch (c) {
-                case 'A':
-                    Alpha_Test.Start();
-                    break;
-                case 'B':
-                    Beta_Test Beta = new Beta_Test();
-                    break;
-
-                case 'E':
-                    System.out.println("Exit");
-                    return;
-                default:
-                    System.out.println("Wrong input");
-                    return;
-            }
-
-        }
-    }
-
     public static void main(String args[]) throws IOException {
         launch(args);
     }
